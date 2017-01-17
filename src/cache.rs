557ac470c1e455322f51d476e894c068bf029d9c
David Schwarzmann <david.schwarzmann@paessler.com>
2017-01-17 11:58:28 +0100

    [ADD] Improved cache handling

--- src/cache.rs
+++ src/cache.rs
@@ -1,10 +1,24 @@
-use std::time::{SystemTime, Duration};
+use std::fs::{File, OpenOptions, create_dir_all};
+use std::io::{Read, Write};
+use std::os::unix::fs::OpenOptionsExt;
+use std::env;
+
+use std::time::{Instant, SystemTime};
+
+use self::CacheType::*;
 
 pub struct CacheEntry {
-    server: String,
-    username: String,
-    token: String,
-    expires_at: SystemTime
+    pub server: String,
+    pub username: String,
+    pub token: String,
+    pub expires_at: Instant
+}
+
+pub struct Cache {
+    pub file_name: String,
+    pub file: File,
+    pub cache_type: CacheType,
+    pub created_at: Instant
 }
 
 pub enum CacheError {
@@ -12,26 +26,80 @@ pub enum CacheError {
     InternalError,
 }
 
-pub fn init() {
-
+pub enum CacheType {
+    TimeBased,
+    Persistent
 }
 
-pub fn contains_server(server: String) -> bool {
+pub fn init(cache_type: CacheType) -> Cache {
+    let mut dir : String;
 
+    match cache_type {
+        TimeBased => {
+            let mut p = env::temp_dir();
+            p.push("cache");
+            dir = p.to_str().unwrap().to_owned();
+        },
+        Persistent => {
+            let mut p = env::home_dir().unwrap();
+            p.push(".ci-login");
+            dir = p.to_str().unwrap().to_owned();
+            create_dir_all(&dir);
+            dir.push_str("/cache");
+        }
+    }
+
+    println!("{:?}", &dir);
+
+    let file = OpenOptions::new()
+            .mode(0o640)
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&dir).unwrap();
+
+    return Cache {
+        file_name: dir,
+        file: file,
+        cache_type: cache_type,
+        created_at: Instant::now()
+    };
 }
 
-pub fn contains_entry(entry: CacheEntry) -> bool {
+impl Cache {
+    pub fn contains_server(&self, server: &String) -> bool {
+        let buffer = Vec::new();
+        let length = self.file.read_to_end(&mut buffer);
+        return false;
+    }
 
-}
+    pub fn contains_entry(&self, entry: CacheEntry) -> bool {
+        unimplemented!()
+    }
 
-pub fn add_entry(entry: CacheEntry) -> Result<bool, CacheError> {
+    pub fn add_entry(&self, entry: CacheEntry) -> Result<bool, CacheError> {
+        // Check if entry already exists
 
-}
+        // Update if it exists
+        // Create if not exists
+        // Encode base64
+        // format!("{}:{}", username, password);
+        unimplemented!()
+    }
 
-pub fn remove_entry(server: String) -> Result<bool, CacheError> {
+    pub fn remove_entry(&self, server: String) -> Result<bool, CacheError> {
+        unimplemented!()
+    }
 
-}
+    pub fn get_entry(&self, server: String) -> CacheEntry {
+        unimplemented!()
+    }
 
-pub fn get_entry(server: String) -> CacheEntry {
+    fn save(&self) -> Result<(), CacheError> {
+        Err(());
+    }
 
+    fn load_file(&self) {
+
+    }
 }
